b52ca068f9e0bd6d8ba96b41348d2672e59461b1
knightspore <cslemon96@gmail.com>
2020-07-21 20:00:48 +0200

    Basic functionality working

--- h1light.js
+++ h1light.js
@@ -4,31 +4,25 @@ var elHTML;
 var col;
 var el;
 
-function addPrefix() {
-    heading.forEach(function(hX) {
+heading.forEach(function(hX) {
 
-        el = hX.nodeName;
-    
-        if (el == 'H1') {
-            col = 'red';
-        } else if (el == 'H2') {
-            col = 'orange';
-        } else if (el == 'H3') {
-            col = 'yellow';
-        } else if (el == 'H4') {
-            col = 'lime';
-        } else if (el == 'H5') {
-            col = 'blue';
-        } else if (el == 'H6') {
-            col = 'indigo';
-        }
-    
-        elHTML = hX.innerHTML;
-        append = '<span style="color: ' + col + ';">' + el + ' > </span> ';
-        hX.innerHTML = (append + elHTML);
-    
-        console.log(elHTML);
-    });
-}
+            el = hX.nodeName;
 
+            if (el == 'H1') {
+                col = 'red';
+            } else if (el == 'H2') {
+                col = 'orange';
+            } else if (el == 'H3') {
+                col = 'yellow';
+            } else if (el == 'H4') {
+                col = 'lime';
+            } else if (el == 'H5') {
+                col = 'blue';
+            } else if (el == 'H6') {
+                col = 'indigo';
+            };
 
+            elHTML = hX.innerHTML;
+            append = '<span style="color: ' + col + ';">' + el + ' > </span> ';
+            hX.innerHTML = (append + elHTML);
+        });
